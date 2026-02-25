aea9c3756a8a4de829f680301d9aa8c58315c464
love chaudhary <lovechaudharyji@users.noreply.github.com>
2026-02-25 17:12:36 +0530

    chore_use_3S_png

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function Home() {
           }}
         >
           <Image
-            src="/image/3.png"
+            src="/image/3S.png"
             alt="Spirit Watch"
             width={1400}
             height={1400}
