24b4836a2fbc395b3aa0533c77a661d748fc1a89
love chaudhary <lovechaudharyji@users.noreply.github.com>
2026-02-25 17:41:33 +0530

    style_increase_suprans_watch_size

--- app/page.tsx
+++ app/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
             width={1200}
             height={1200}
             className="max-h-[85vh] w-auto object-contain"
-            style={{ transform: 'scale(1.5)' }}
+            style={{ transform: 'scale(1.7)' }}
           />
         </div>
       </div>
